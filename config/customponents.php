96e6f6fd65af3541d3b45288be984b76f52a3c08
Seddor Ephraim <seddorephraim7@gmail.com>
2021-09-07 03:27:53 +0100

    Install mawuekom/laravel-custom-view-components package

---
+++ config/customponents.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /**
+     * App Name should be set here
+     */
+
+    'app_name' => env('APP_NAME', 'My Laravel Project'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Bunch of features to enable or disable.
+    |--------------------------------------------------------------------------
+    */
+
+    'enable' => [
+        'app_style_sheet'   => true,
+        'app_script'        => true,
+    ],
+];
